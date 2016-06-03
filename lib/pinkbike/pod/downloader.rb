d45ae869c08d67427b09c68c8954fbc18e9308bf
Joe Nyland <joenyland@me.com>
2016-06-03 15:03:20 +0100

    Initial commit

---
+++ lib/pinkbike/pod/downloader.rb
@@ -0,0 +1,9 @@
+require "pinkbike/pod/downloader/version"
+
+module Pinkbike
+  module Pod
+    module Downloader
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/pinkbike/pod/downloader/version.rb
@@ -0,0 +1,7 @@
+module Pinkbike
+  module Pod
+    module Downloader
+      VERSION = "0.1.0"
+    end
+  end
+end
